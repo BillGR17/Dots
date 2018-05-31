f905710d20399d6a6e3485cd5abd72d9ba86f676
Bill Dallas <bill_d17@hotmail.com>
2018-05-31 19:14:58 +0300

    Changed Bandwidth Removed bash script that i didnt create and created my on in c++

---
+++ .config/i3/blocks/scripts/cpp/bandwidth.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+void format(float bytes){
+  std::cout<<std::fixed;
+  if(bytes < 1024){
+    std::cout<<std::setprecision(0) << bytes<<" b ";
+  }else if(bytes < 1048576){
+    std::cout<<std::setprecision(0) << (bytes / 1024)<<" kb ";
+  }else if(bytes < 1073741824){
+    std::cout<<std::setprecision(2) << (bytes / 1048576)<<" mb ";
+  }else {
+    std::cout<<std::setprecision(2) << (bytes / 1073741824)<<" gb ";
+  }
+}
+
+int main(){
+  system("ip route | awk '/^default/ { print $5 ; exit }'>/tmp/.net_con");
+  std::string toExec="cat /proc/net/dev | grep $(cat /tmp/.net_con) | awk '{print $2 \" \" $3}'";
+  std::string bw=exec(toExec.c_str());
+  std::string obw=exec("cat /tmp/.net_bw");
+  system("cat /proc/net/dev | grep $(cat /tmp/.net_con) | awk '{print $2 \" \" $3}'>/tmp/.net_bw&");
+  int down = std::stoll(bw.substr(0,bw.find(" "))) - std::stoll(obw.substr(0,obw.find(" ")));
+  int up = std::stoll(bw.substr(bw.find(" "),bw.length())) - std::stoll(obw.substr(obw.find(" "),obw.length()));
+  std::cout<<" ";
+  format(down);
+  std::cout<<" ";
+  format(up);
+  return 0;
+}
