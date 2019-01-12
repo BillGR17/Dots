86b8c51117fc14bcda94c1203d6207fad5b9fc61
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2019-01-12 16:53:34 +0000

    added Conf script in C

---
+++ WorkPlace/Scripts/conf.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+char h[]="Specify What you need to confing\n[ng=nginx]\n[ap=apache]\n[i3=i3wm]\n[vi=nvim]\n[ra=ranger]\n[tm=tmux]\n[zh=zsh]\n[xr=xterm]\n";
+void exec(char x[]){
+  char c[100];
+  sprintf(c,"nvim %s",x);
+  system(c);
+}
+void list(char s[]){
+  if(strcmp(s,"i3")==0){
+    exec("~/.config/i3/config");
+  }else if(strcmp(s,"vi")==0){
+    exec("~/.config/nvim/init.vim");
+  }else if(strcmp(s,"ng")==0){
+    exec("/etc/nginx/nginx.conf&&systemctl restart nginx.service");
+  }else if(strcmp(s,"ap")==0){
+    exec("/etc/httpd/conf/httpd.conf&&systemctl restart httpd.service");
+  }else if(strcmp(s,"tm")==0){
+    exec("~/.tmux.conf");
+  }else if(strcmp(s,"zh")==0){
+    exec("~/.zshrc");
+  }else if(strcmp(s,"xr")==0){
+    exec("~/.Xresources&&xrdb -merge ~/.Xresources");
+  }else{
+    puts(h);
+  }
+}
+int main(int argc, char *argv[]){
+  if(argc==2){
+    list(argv[1]);
+  }else{
+    puts(h);
+    return 1;
+  }
+  return 0;
+}
