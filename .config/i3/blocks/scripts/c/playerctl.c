22537d745d4235cd9fd62a6406b77852fcf437ea
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2019-01-25 01:48:18 +0200

    Added playerctl in C

---
+++ .config/i3/blocks/scripts/c/playerctl.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *exec(const char *x){
+  char b[128],*r=malloc(256*sizeof(*r));
+  FILE *cmd=popen(x,"r");
+  if (cmd == NULL){
+    perror("Error while opening the file.\n");
+    exit(2);
+  }
+  while(fgets(b,128,cmd)){
+    strcpy(r,strtok(b,"\n"));
+  }
+  pclose(cmd);
+  return r;
+}
+
+int main(){
+  char *e=getenv("BLOCK_INSTANCE");
+  if(e==NULL){
+    exit(1);
+  }
+  char x[60]="playerctl -p ";
+  strcat(x,e);
+  char s[60];
+  strcpy(s,x);
+  strcat(s," status");
+  const char *status=exec(s);
+  if(strcmp(status,"No players found")!=0){
+    strcat(x," metadata --format '{{ artist }} ~ {{ title }}'");
+    const char *info=exec(x);
+    if(strstr(status,"Playing")!=NULL){
+      printf(" %s\n",info);
+      fflush(stdout);
+    }else if(strstr(status,"Paused")!=NULL){
+      printf(" %s\n",info);
+      fflush(stdout);
+    }
+  }
+  return 0;
+}
