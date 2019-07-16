6c2493ce96cbe78cea7ebedbb96afef92611539d
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2019-07-16 22:40:12 +0300

    gets info from /sys/ instead of /proc/

--- .config/i3/blocks/scripts/c/bandwidth.c
+++ .config/i3/blocks/scripts/c/bandwidth.c
@@ -4,35 +4,27 @@
 #include <unistd.h>
 long long int u=0,d=0;
 
-long long int grep(const char *x,int l){
-  char ln[255];
+long long int readf(const char *x){
   long long int r=0;
-  FILE *fp = fopen("/proc/net/dev", "r");  
+  FILE *fp = fopen(x, "r");
   if (fp == NULL){
     perror("Error while opening the file.\n");
     exit(EXIT_FAILURE);
   }
-  while(fgets(ln,255,fp)){
-    if(strstr(ln,x)!=0){
-      char *p = strtok (ln, " ");
-      int i=0;
-      char *array[20]; 
-      while (p != NULL){
-        array[i++] = p;
-        p = strtok (NULL, " ");
-      }
-      r=atoll(array[l]);
-    }
-  }
+  fscanf(fp,"%lld",&r);
   fclose(fp);
   return r;
 }
 
 int main(){
   const char *e=getenv("BLOCK_INSTANCE");
+  char tu[256];
+  char td[256];
+  sprintf(td,"/sys/class/net/%s/statistics/rx_bytes",e);
+  sprintf(tu,"/sys/class/net/%s/statistics/tx_bytes",e);
   long long int _u=0,_d=0;
   while(1){
-    d=grep(e,1);u=grep(e,9);
+    d=readf(td);u=readf(tu);
     if(_d>0){
       printf(":%.2fmb :%.2fmb",(float)(d-_d)/1024/1024,(float)(u-_u)/1024/1024);
       fflush(stdout);
