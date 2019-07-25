9a016909f5ed28c612ad3c7be77c18c9752ff02a
Bill Dallas <11507104+BillGR17@users.noreply.github.com>
2019-07-25 16:15:57 +0300

    Created audio script

---
+++ .config/i3/blocks/scripts/c/audio.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  char *e=getenv("BLOCK_BUTTON");
+  if(e==NULL){
+    exit(1);
+  }
+  int m = atoi(e);
+  switch (m) {
+    case 1:
+      system("amixer -q sset Master,0 toggle");
+      break;
+    case 4:
+      system("amixer -q sset Master 1%+ unmute");
+      break;
+    case 5:
+      system("amixer -q sset Master 1%- unmute");
+      break;
+  }
+  system("if [[ $(amixer get Master|grep -Fc \"[off]\") == 0 ]];then echo -e ' '$(amixer get Master|grep -E -o '[0-9]{1,3}?%');else echo -e ' 0';fi");
+  return 0;
+}
