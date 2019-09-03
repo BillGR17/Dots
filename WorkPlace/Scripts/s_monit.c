676ad9361d22f6c3a80668829968c56e988e4275
Bill Dallas <11507104+BillGR17@users.noreply.github.com>
2019-09-03 10:14:08 +0300

    send message when server is unreachable

--- WorkPlace/Scripts/s_monit.c
+++ WorkPlace/Scripts/s_monit.c
@@ -5,12 +5,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <curl/curl.h>
-
 //hide all response text
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp){
   return size * nmemb;
 }
-
 //Put all the info here
 #define WEBSITE "example.com" //the protocol is used on F_WEBSITE
 #define F_WEBSITE "https://" WEBSITE //Protocol Only!
@@ -19,21 +17,12 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp){
 #define ADDR_PASS "pass"//The senders Password
 #define TO_ADDR "<receiver@email.com>"//Enter the Receiver email with < >
 #define SMTP_SERVER "smtp://mail.examplesmtp.com:587"
-
-char *payload_text[768];
-
-struct upload_status {
-  int lines_read;
-};
-
-static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp){
-  struct upload_status *upload_ctx = (struct upload_status *)userp;
-  const char *data;
-  if((size == 0) || (nmemb == 0) || ((size*nmemb) < 1)) {
-    return 0;
-  }
+static size_t payload_source(void* ptr, size_t size, size_t nmemb, void* userp) {
+  struct upload_status* upload_ctx = (struct upload_status*)userp;
+  const char* data;
+  if ((size == 0) || (nmemb == 0) || ((size * nmemb) < 1)) { return 0; }
   data = payload_text[upload_ctx->lines_read];
-  if(data) {
+  if (data) {
     size_t len = strlen(data);
     memcpy(ptr, data, len);
     upload_ctx->lines_read++;
@@ -41,42 +30,40 @@ static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp){
   }
   return 0;
 }
-
-void GetDate(char *Date,int size){
+void GetDate(char* Date, int size) {
   time_t rawtime;
-  struct tm * timeinfo;
-  time ( &rawtime );
-  timeinfo = localtime (&rawtime);
-  strftime(Date,size, "DATE: %a, %d %b %Y %H:%M:%S %z", timeinfo);
+  struct tm* timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(Date, size, "DATE: %a, %d %b %Y %H:%M:%S %z", timeinfo);
 }
-
-void sendMail(char *alert,long int status,double t){
-  CURL *curl = curl_easy_init();
-  if(curl) {
+void sendMail(char* alert, long int status, double t) {
+  CURL* curl = curl_easy_init();
+  if (curl) {
     struct upload_status upload_ctx;
     upload_ctx.lines_read = 0;
     char buffer[100];
-    GetDate(buffer,sizeof(buffer));
-    payload_text[0]=malloc(sizeof(buffer)*sizeof(char));
-    strncpy(payload_text[0],buffer,100);
-    payload_text[1]="To: " TO_ADDR "\r\n";
-    payload_text[2]="From: " FROM_ADDR "\r\n";
-    sprintf(buffer,"Subject: %s %s\r\n",alert,WEBSITE);
-    payload_text[3]=malloc(sizeof(buffer)*sizeof(char));
-    strncpy(payload_text[3],buffer,100);
-    payload_text[4]="\r\n";
-    payload_text[5]=malloc(sizeof(buffer)*sizeof(char));
-    if(t!=0){
+    GetDate(buffer, sizeof(buffer));
+    payload_text[0] = malloc(sizeof(buffer) * sizeof(char));
+    strncpy(payload_text[0], buffer, 100);
+    payload_text[1] = "To: " TO_ADDR "\r\n";
+    payload_text[2] = "From: " FROM_ADDR "\r\n";
+    sprintf(buffer, "Subject: %s %s\r\n", alert, WEBSITE);
+    payload_text[3] = malloc(sizeof(buffer) * sizeof(char));
+    strncpy(payload_text[3], buffer, 100);
+    payload_text[4] = "\r\n";
+    payload_text[5] = malloc(sizeof(buffer) * sizeof(char));
+    if (t != 0) {
       int h = ((double)t / 3600);
-      int m = ((double)t -(3600*h))/60;
-      int s = ((double)t -(3600*h)-(m*60));
-      sprintf(buffer,"The Site is back online after: %.2d:%.2d:%.2d\r\n",h,m,s);
-    }else{
-      sprintf(buffer,"The Site returns code status : %li\r\n",status);
+      int m = ((double)t - (3600 * h)) / 60;
+      int s = ((double)t - (3600 * h) - (m * 60));
+      sprintf(buffer, "The Site is back online after: %.2d:%.2d:%.2d\r\n", h, m, s);
+    } else {
+      sprintf(buffer, "The Site returns code status : %li\r\n", status);
     }
-    strncpy(payload_text[5],buffer,100);
-    payload_text[6]="\r\n";
-    struct curl_slist *recipients = NULL;
+    strncpy(payload_text[5], buffer, 100);
+    payload_text[6] = "\r\n";
+    struct curl_slist* recipients = NULL;
     curl_easy_setopt(curl, CURLOPT_USERNAME, N_FROM_ADDR);
     curl_easy_setopt(curl, CURLOPT_PASSWORD, ADDR_PASS);
     curl_easy_setopt(curl, CURLOPT_URL, SMTP_SERVER);
@@ -88,8 +75,7 @@ void sendMail(char *alert,long int status,double t){
     curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
     CURLcode res = CURLE_OK;
     res = curl_easy_perform(curl);
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    if (res != CURLE_OK) fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
     curl_slist_free_all(recipients);
     curl_easy_cleanup(curl);
     free(payload_text[3]);
@@ -98,36 +84,40 @@ void sendMail(char *alert,long int status,double t){
 }
 int err;
 time_t s_t, e_t;
-
-void setErr(int val){
-  err=val;
+void setErr(int val) {
+  err = val;
 }
-
-void checkPage(int err){
-  CURL *curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL,F_WEBSITE);
+void checkPage(int err) {
+  CURL* curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, F_WEBSITE);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-    if(curl_easy_perform(curl) == CURLE_OK){
-      long http_code=0;
+    if (curl_easy_perform(curl) == CURLE_OK) {
+      long http_code = 0;
       curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-      if(http_code>399&&err==0) {
+      printf("%ld %d\n", http_code, err);
+      if (http_code != 200 && err == 0) {
         setErr(1);
         time(&s_t);
-        sendMail("Website is Down",http_code,0);
+        sendMail("Website is Down", http_code, 0);
       }
-      if(http_code<400&&err==1){
+      if (http_code == 200 && err == 1) {
         setErr(0);
         time(&e_t);
-        sendMail("Website is Up",http_code,(double)difftime(e_t, s_t));
+        sendMail("Website is Up", http_code, (double)difftime(e_t, s_t));
+      }
+    } else {
+      if (err == 0) {
+        setErr(1);
+        time(&s_t);
+        sendMail("Website is Down server cant be reached!", 500, 0);
       }
     }
     curl_easy_cleanup(curl);
   }
 }
-
-int main(void){
-  while(1){
+int main(void) {
+  while (1) {
     checkPage(err);
     sleep(1);
   }
