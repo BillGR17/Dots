f0503c7539b14f3c680409996dfe3d9558edcb39
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2020-01-15 17:09:54 +0200

    Removed gtk lib

--- .config/i3/blocks/scripts/c/shutdown.c
+++ .config/i3/blocks/scripts/c/shutdown.c
@@ -1,33 +1,60 @@
-#include <gtk/gtk.h>
 #include <stdio.h>
-static void Shutdown(GtkWidget* widget, gpointer data) {
-  system("poweroff");
+#include <stdlib.h>
+#include <string.h>
+#include <X11/Xlib.h>
+Display* d;
+Window w;
+XEvent e;
+XGCValues v;
+XFontStruct* f;
+Atom a;
+GC gc;
+short int s, terminate = 0, w_w = 400, w_h = 200;
+void onError(char* t, int c) {
+  fprintf(stderr, "%s\n", t);
+  exit(c);
 }
-static void activate(GtkApplication* app, gpointer user_data) {
-  GtkWidget* window;
-  GtkWidget* button;
-  GtkWidget* button2;
-  GtkWidget* button_box;
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Shutdown PC");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-  button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add(GTK_CONTAINER(window), button_box);
-  button2 = gtk_button_new_with_label("Shutdown");
-  g_signal_connect(button2, "clicked", G_CALLBACK(Shutdown), NULL);
-  gtk_container_add(GTK_CONTAINER(button_box), button2);
-  button = gtk_button_new_with_label("Exit");
-  g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-  gtk_container_add(GTK_CONTAINER(button_box), button);
-  gtk_button_box_set_layout(button_box, GTK_BUTTONBOX_EXPAND);
-  gtk_widget_show_all(window);
+void button(char* txt, int x, int y, void (*func)()) {
+  int xx = w_w / 2;
+  XDrawRectangle(d, w, gc, x, y, xx, w_h);
+  XDrawString(d, w, gc, x + 3, y + w_h / 2, txt, strlen(txt));
+  int r_x, r_y, wm_x, wm_y;
+  Window r_w, r_c;
+  unsigned int m;
+  XQueryPointer(d, w, &r_w, &r_c, &r_x, &r_y, &wm_x, &wm_y, &m);
+  if ((x < wm_x && (x + xx) > wm_x) && (y < wm_y && (y + w_h) > wm_y) && (e.type == ButtonRelease)) { func(d); }
 }
-int main(int argc, char** argv) {
-  GtkApplication* app;
-  int status;
-  app = gtk_application_new("org.gtk.shutitdown", GTK_WINDOW_TOPLEVEL);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
-  return status;
+void printit() {
+  system("/usr/bin/poweroff");
+}
+void destroy() {
+  terminate = 1;
+}
+int handler(Display* d, XErrorEvent* e) {
+  char msg[256];
+  XGetErrorText(d, e->error_code, msg, 256);
+  fprintf(stderr, "%s\n", msg);
+  return e->type;
+}
+int main(void) {
+  XSetErrorHandler(handler);
+  d = XOpenDisplay(NULL);
+  if (d == NULL) { onError("Cant open display", 1); }
+  f = XLoadQueryFont(d, "*-hack-medium-r-normal-*");
+  if (d == NULL) { onError("Cant load font", 2); }
+  s = DefaultScreen(d);
+  w = XCreateSimpleWindow(d, RootWindow(d, s), 0, 0, w_w, w_h, 1, BlackPixel(d, s), WhitePixel(d, s));
+  gc = XCreateGC(d, w, 0, &v);
+  XMapWindow(d, w);
+  XSetFont(d, gc, f->fid);
+  XStoreName(d, w, "Shutdown PC");
+  XSelectInput(d, w, ExposureMask | KeyPressMask | ButtonReleaseMask | ButtonPressMask);
+  while (1) {
+    if (e.type == KeyPress || terminate) break;
+    XNextEvent(d, &e);
+    button("System Shutdown", 0, 0, printit);
+    button("Close Window", 200, 0, destroy);
+  }
+  XCloseDisplay(d);
+  return 0;
 }
