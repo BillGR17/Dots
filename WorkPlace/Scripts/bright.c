610f36dbd14a20a377883758c197e2de5f930fcb
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2020-01-24 17:08:24 +0200

    [UNTESTED]Changed chars to double & added arg to change percentage

--- WorkPlace/Scripts/bright.c
+++ WorkPlace/Scripts/bright.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,17 +13,19 @@ char card[128];
 // another variables to
 // increase
 char dec = 0;
-// could change char r to int or float since
-// we only get int values from brightness
-void readFile(char* e, char* r, int size) {
+// how much percentage
+// to change
+// the brightness
+int percentage = 1;
+void readFile(char* e, double r) {
   FILE* f = fopen(e, "r");
-  char buf[size];
+  char buf[128];
   if (f == NULL) {
     fprintf(stderr, "Could not open file %s\n", e);
     exit(2);
   }
   if (fgets(buf, sizeof(buf), f) != NULL) {
-    strncpy(r, buf, sizeof(buf));
+    r = atof(buf);
   } else {
     puts("Could not find shit in file\n");
     exit(3);
@@ -43,44 +46,51 @@ void writeFile(char* e, int x) {
  */
 void args(int argc, char* argv[]) {
   int opt;
-  while ((opt = getopt(argc, argv, "c:d")) != -1) {
+  while ((opt = getopt(argc, argv, "c:dv:")) != -1) {
     switch (opt) {
       case 'c': strncpy(card, optarg, sizeof(card)); break;
       case 'd': dec = 1; break;
+      case 'v':
+        percentage = atoi(optarg);
+        // atoi returns 0 instead of error so i think
+        // we are cool using it
+        if (percentage < 1) { percentage = 1; }
+        break;
     }
   }
 }
 int main(int argc, char* argv[]) {
   args(argc, argv);
   if (strlen(card) == 0) {
-    fprintf(stderr, "All you had to do was to provide the god damn card!\n");
-    puts("Usage: bright -c \"intel_backlight\" -d\nIf you want to increase you only need to type the card name\n");
+    fprintf(
+      stderr,
+      "Usage: bright -c \"intel_backlight\" -d -v 5\nIf you want to increase you only need to type the card name\n"
+      "\nAll you had to do was to provide the god damn card!\n");
     exit(1);
   }
   // get max brightness path
-  char p_m[256];
+  char p_m[128];
   sprintf(p_m, "/sys/class/backlight/%s/max_brightness", card);
   // get current brightness path
-  char p_c[256];
+  char p_c[128];
   sprintf(p_c, "/sys/class/backlight/%s/brightness", card);
   // get max brightness
-  char m_b[128];
-  readFile(p_m, m_b, sizeof(m_b));
+  double m_b = 0;
+  readFile(p_m, m_b);
   // get current brightness
-  char c_b[128];
-  readFile(p_c, c_b, sizeof(c_b));
-  // calculate the 1 percentage of brightness
-  int c_b_p_n = ((double)1 / (double)100) * atof(m_b);
-  // now read button events and increase or decrease the brightness by 5%
-  int x = atoi(c_b);
+  double c_b = 0;
+  readFile(p_c, c_b);
+  // calculate the percentage of brightness
+  int c_b_p_n = ((double)percentage / (double)100) * m_b;
+  // now read button events and increase or decrease the brightness
   if (!dec) {
-    x += c_b_p_n;
-    if (x > atoi(m_b)) { x = atoi(m_b); }
-    writeFile(p_c, x);
+    c_b += c_b_p_n;
+    if (c_b > m_b) { c_b = m_b; }
+    writeFile(p_c, (int)c_b);
   } else {
-    x -= c_b_p_n;
-    if (x <= 0) { x = 1; }
-    writeFile(p_c, x);
+    c_b -= c_b_p_n;
+    if (c_b <= 0) { c_b = 1; }
+    writeFile(p_c, (int)c_b);
   }
   return 0;
 }
