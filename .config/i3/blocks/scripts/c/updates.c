ba48183b25ff9b596f7415834fa16885e10c7d92
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2023-04-08 17:25:45 +0300

    dont check on before update

--- .config/i3/blocks/scripts/c/updates.c
+++ .config/i3/blocks/scripts/c/updates.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 int main() {
   char* e = getenv("BLOCK_BUTTON");
-  // check for updates by default
-  system("checkupdates > /tmp/updates_list;yay -Qua >> /tmp/updates_list;cat /tmp/updates_list|wc -l");
-  // if a button has been pressed run different commands
-  if (e != NULL) {
-    int m = atoi(e);
-    switch (m) {
-      // since middle click is less accidentally pressed
-      // it should run the yay command which now shows all packages before upgrading
-      case 2: system("xterm -e zsh -c \"yay -Syu&&echo 'All Done :)'&&read\""); break;
-    }
+  int m = 0;
+  if (e != NULL) m = atoi(e);
+  switch (m) {
+    // since middle click is less accidentally pressed
+    // it should run the yay command which now shows all packages before upgrading
+    case 2:
+      system("xterm -e zsh -c \"yay -Syu&&echo 'All Done :)'&&read\"");
+      break;
+      // check for updates by default
+    default: system("checkupdates > /tmp/updates_list;yay -Qua >> /tmp/updates_list;cat /tmp/updates_list|wc -l");
   }
   return 0;
 }
