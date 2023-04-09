5d990953059eae6e5d6a0f86889bf942d175b1a9
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2023-04-09 22:59:25 +0300

    show 0 after yay upgrade

--- .config/i3/blocks/scripts/c/updates.c
+++ .config/i3/blocks/scripts/c/updates.c
@@ -9,6 +9,9 @@ int main() {
     // it should run the yay command which now shows all packages before upgrading
     case 2:
       system("xterm -e zsh -c \"yay -Syu&&echo 'All Done :)'&&read\"");
+      // since we just run upgrade command just throw 0 packages
+      // to show something on i3block
+      puts("0");
       break;
       // check for updates by default
     default: system("checkupdates > /tmp/updates_list;yay -Qua >> /tmp/updates_list;cat /tmp/updates_list|wc -l");
