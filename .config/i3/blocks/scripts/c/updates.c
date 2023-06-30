40d8d7f13d98b0fb69bdff3e1b0701439cc9f44b
Bill Dallas <11507104+BillGR17@users.noreply.github.com>
2023-06-30 13:27:22 +0300

    Some improvements and minor changes
    After middle click is pressed  it will perform the updates as usual but after the updates are done it will check and refresh the list instead of showing 0 no matter what

--- .config/i3/blocks/scripts/c/updates.c
+++ .config/i3/blocks/scripts/c/updates.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+void checkupdates() {
+  system("checkupdates > /tmp/updates_list;yay -Qua >> /tmp/updates_list");
+}
+int getlines(const char* filename) {
+  FILE* fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("fopen");
+    exit(EXIT_FAILURE);
+  }
+  int i = 0;
+  char c;
+  for (c = getc(fp); c != EOF; c = getc(fp))
+    if (c == '\n') // Increment count if this character is newline
+      i = i + 1;
+  fclose(fp);
+  return i;
+}
 int main() {
   char* e = getenv("BLOCK_BUTTON");
   int m = 0;
   if (e != NULL) m = atoi(e);
+  int updates = 0;
   switch (m) {
     // since middle click is less accidentally pressed
     // it should run the yay command which now shows all packages before upgrading
     case 2:
-      system("xterm -e zsh -c \"yay -Syu&&echo 'All Done :)'&&read\"");
-      // since we just run upgrade command just throw 0 packages
-      // to show something on i3block
-      puts("0");
+      system("xterm -e zsh -c \"cat /tmp/updates_list&&yay -Syu&&echo 'All Done :)'&&read\"");
+      // check again for updates and update
+      // run the results for the i3block once more
+      checkupdates();
+      updates = getlines("/tmp/updates_list");
+      printf("%d\n", updates);
       break;
+    default:
       // check for updates by default
-    default: system("checkupdates > /tmp/updates_list;yay -Qua >> /tmp/updates_list;cat /tmp/updates_list|wc -l");
+      checkupdates();
+      updates = getlines("/tmp/updates_list");
+      printf("%d\n", updates);
   }
   return 0;
 }
