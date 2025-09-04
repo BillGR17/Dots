46c9145d8ec69a6c850552b21f71a508e3dd91e4
Bill Dallas <11507104+BillGR17@users.noreply.github.com>
2025-09-05 01:57:25 +0300

    Minor Improvements

--- WorkPlace/Scripts/helpers/bright.c
+++ WorkPlace/Scripts/helpers/bright.c
@@ -3,24 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-// card will set the
-// current device/direction to
-// get/set the brightness
+
+// card will set the current device
 char card[128];
-// so basically this is the
-// dec 0 means false
-// there is no need for
-// another variables to
-// increase
-char dec = 0;
-// how much percentage
-// to change
-// the brightness
+char dec = 0; // 0 for increase, 1 for decrease
 int percentage = 1;
-int readFile(char* e) {
+
+// Checks the card name for dangerous characters.
+void sanitize_card(const char* input_card) {
+  if (strchr(input_card, '/') != NULL || strchr(input_card, '.') != NULL) {
+    fprintf(stderr, "Error: Invalid card name. The characters '/' and '.' are not allowed.\n");
+    exit(10);
+  }
+}
+
+int readFile(const char* e) {
   FILE* f = fopen(e, "r");
   char buf[128];
-  double r;
+  int r = 0;
   if (f == NULL) {
     fprintf(stderr, "Could not open file %s\n", e);
     exit(2);
@@ -28,69 +28,73 @@ int readFile(char* e) {
   if (fgets(buf, sizeof(buf), f) != NULL) {
     r = atoi(buf);
   } else {
-    puts("Could not find shit in file\n");
+    fprintf(stderr, "Could not find data in file %s\n", e);
     exit(3);
   }
   fclose(f);
   return r;
 }
-void writeFile(char* e, int x) {
-  FILE* f = fopen(e, "w+");
+
+void writeFile(const char* e, int x) {
+  FILE* f = fopen(e, "w");
   if (f == NULL) {
-    fprintf(stderr, "Could not open file %s\n", e);
+    perror("Could not open file for writing");
     exit(2);
   }
   fprintf(f, "%d", x);
   fclose(f);
 }
-/*
- * Checks the arguments then it will change the files
- */
+
 void args(int argc, char* argv[]) {
   int opt;
   while ((opt = getopt(argc, argv, "c:dv:")) != -1) {
     switch (opt) {
-      case 'c': strncpy(card, optarg, sizeof(card)); break;
+      case 'c':
+        sanitize_card(optarg); // We sanitize the input
+        strncpy(card, optarg, sizeof(card) - 1);
+        card[sizeof(card) - 1] = '\0'; // Ensure null-termination
+        break;
       case 'd': dec = 1; break;
       case 'v':
         percentage = atoi(optarg);
-        // atoi returns 0 instead of error so i think
-        // we are cool using it
         if (percentage < 1) { percentage = 1; }
         break;
     }
   }
 }
+
 int main(int argc, char* argv[]) {
   args(argc, argv);
+
   if (strlen(card) == 0) {
-    fprintf(
-      stderr,
-      "Usage: bright -c \"intel_backlight\" -d -v 5\nIf you want to increase you only need to type the card name\n"
-      "\nAll you had to do was to provide the god damn card!\n");
+    fprintf(stderr, "Usage: %s -c \"intel_backlight\" [-d] [-v 5]\n", argv[0]);
     exit(1);
   }
-  // get max brightness path
-  char p_m[128];
-  sprintf(p_m, "/sys/class/backlight/%s/max_brightness", card);
-  // get current brightness path
-  char p_c[128];
-  sprintf(p_c, "/sys/class/backlight/%s/brightness", card);
-  // get max brightness
+
+  char p_m[256];
+  snprintf(p_m, sizeof(p_m), "/sys/class/backlight/%s/max_brightness", card);
+
+  char p_c[256];
+  snprintf(p_c, sizeof(p_c), "/sys/class/backlight/%s/brightness", card);
+
   double m_b = readFile(p_m);
-  // get current brightness
   double c_b = readFile(p_c);
-  // calculate the percentage of brightness
-  double c_b_p_n = ((double)percentage / (double)100) * m_b;
-  // now read button events and increase or decrease the brightness
+
+  double c_b_p_n = (percentage / 100.0) * m_b;
+
   if (!dec) {
     c_b += c_b_p_n;
     if (c_b > m_b) { c_b = m_b; }
-    writeFile(p_c, (int)c_b);
   } else {
     c_b -= c_b_p_n;
-    if (c_b <= 0) { c_b = 1; }
-    writeFile(p_c, (int)c_b);
+    if (c_b <= 0) {
+      c_b = 1; // Brightness should not be 0
+    }
   }
+
+  writeFile(p_c, (int)c_b);
+
+  if (setuid(getuid()) != 0) { perror("Failed to drop privileges"); }
+
   return 0;
 }
