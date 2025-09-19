819984530ebaec2a9b30a4f97308c37a46993a6e
Bill Dallas <11507104+BillGR17@users.noreply.github.com>
2025-09-19 14:49:50 +0300

    Update conf.c
    improved code and added missing shortcuts

--- WorkPlace/Scripts/helpers/conf.c
+++ WorkPlace/Scripts/helpers/conf.c
@@ -1,47 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char h[] = "Specify What you need to "
-           "confing\n[ng=nginx]\n[ap=apache]\n[i3=i3wm]\n[vi=nvim]\n[ra=ranger]\n[tm=tmux]\n[zh=zsh]\n[xr=xterm]\n"
-           "[pi=picom]\n[du=dunst]\n[ro=rofi]";
-void exec(char x[]) {
-  char c[100];
-  sprintf(c, "nvim %s", x);
-  system(c);
-}
-void list(char s[]) {
-  if (strcmp(s, "i3") == 0) {
-    exec("~/.config/i3/config");
-  } else if (strcmp(s, "vi") == 0) {
-    exec("~/.config/nvim/init.vim");
-  } else if (strcmp(s, "ra") == 0) {
-    exec("~/.config/ranger/rc.conf");
-  } else if (strcmp(s, "ro") == 0) {
-    exec("~/.config/rofi/config.rasi");
-  } else if (strcmp(s, "ng") == 0) {
-    exec("/etc/nginx/nginx.conf&&systemctl restart nginx.service");
-  } else if (strcmp(s, "ap") == 0) {
-    exec("/etc/httpd/conf/httpd.conf&&systemctl restart httpd.service");
-  } else if (strcmp(s, "pi") == 0) {
-    exec("~/.config/picom/pi.conf");
-  } else if (strcmp(s, "du") == 0) {
-    exec("~/.config/dunst/dunstrc");
-  } else if (strcmp(s, "tm") == 0) {
-    exec("~/.tmux.conf");
-  } else if (strcmp(s, "zh") == 0) {
-    exec("~/.zshrc");
-  } else if (strcmp(s, "xr") == 0) {
-    exec("~/.Xresources&&xrdb -merge ~/.Xresources");
-  } else {
-    puts(h);
+
+// A struct to hold the command details
+typedef struct {
+  const char* alias;
+  const char* description;
+  const char* command;
+} ConfigCommand;
+
+// Array of all available commands
+// Adding a new one is as simple as adding a new line here.
+static const ConfigCommand commands[] = {
+    {"ap", "apache", "nvim /etc/httpd/conf/httpd.conf && systemctl restart httpd.service"},
+    {"du", "dunst", "nvim ~/.config/dunst/dunstrc"},
+    {"hy", "hyprland", "nvim ~/.config/hypr/hyprland.conf"},
+    {"i3", "i3wm", "nvim ~/.config/i3/config"},
+    {"ng", "nginx", "nvim /etc/nginx/nginx.conf && systemctl restart nginx.service"},
+    {"pi", "picom", "nvim ~/.config/picom/pi.conf"},
+    {"ra", "ranger", "nvim ~/.config/ranger/rc.conf"},
+    {"ro", "rofi", "nvim ~/.config/rofi/config.rasi"},
+    {"tm", "tmux", "nvim ~/.tmux.conf"},
+    {"vi", "nvim", "nvim ~/.config/nvim/init.lua"},
+    {"wb", "waybar", "nvim ~/.config/waybar/config"},
+    {"xr", "xterm", "nvim ~/.Xresources && xrdb -merge ~/.Xresources"},
+    {"zh", "zsh", "nvim ~/.zshrc"},
+};
+
+// Function to print the help message dynamically
+void printHelp() {
+  puts("Specify what you need to configure:\n");
+  // Calculate the number of commands in the array
+  size_t numCommands = sizeof(commands) / sizeof(commands[0]);
+  for (size_t i = 0; i < numCommands; ++i) {
+    printf("  [%s] %s\n", commands[i].alias, commands[i].description);
   }
 }
+
 int main(int argc, char* argv[]) {
-  if (argc == 2) {
-    list(argv[1]);
-  } else {
-    puts(h);
+  // Ensure exactly one argument is provided
+  if (argc != 2) {
+    printHelp();
     return 1;
   }
+
+  const char* inputAlias = argv[1];
+  int commandFound = 0;
+
+  // Loop through the commands array to find a match
+  size_t numCommands = sizeof(commands) / sizeof(commands[0]);
+  for (size_t i = 0; i < numCommands; ++i) {
+    if (strcmp(inputAlias, commands[i].alias) == 0) {
+      system(commands[i].command);
+      commandFound = 1;
+      break; // Exit loop once command is found and executed
+    }
+  }
+
+  // If no match was found after checking all commands, show help
+  if (!commandFound) {
+    fprintf(stderr, "Error: Unknown alias \"%s\"\n\n", inputAlias);
+    printHelp();
+    return 1;
+  }
+
   return 0;
 }
